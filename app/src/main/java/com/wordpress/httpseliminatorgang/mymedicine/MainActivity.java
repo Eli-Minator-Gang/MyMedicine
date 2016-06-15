e95a9ee31eb91e4d1adf3b4a3413ad8fe50192b7
TakedaLeyasu <c.frauendorf2000@web.de>
2016-06-15 15:13:13 +0200

    Requesting Permissions at Run Time
    für Android 6 notwendig

--- app/src/main/java/com/wordpress/httpseliminatorgang/mymedicine/MainActivity.java
+++ app/src/main/java/com/wordpress/httpseliminatorgang/mymedicine/MainActivity.java
@@ -1,15 +1,26 @@
 package com.wordpress.httpseliminatorgang.mymedicine;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CAMERA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +33,42 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, QRCodeActivity.class));
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    // Camera permission has not been granted.
+
+                    requestCameraPermission();
+
+                } else {
+
+                    // Camera permissions is already available, show the camera preview.
+                    startActivity(new Intent(MainActivity.this, QRCodeActivity.class));
+                }
+
             }
         });
+    }
+
+    private void requestCameraPermission() {
+
+        // BEGIN_INCLUDE(camera_permission_request)
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
+            // Provide an additional rationale to the user if the permission was not granted
+            // and the user would benefit from additional context for the use of the permission.
+            // For example if the user has previously denied the permission.
+
+            Toast.makeText(getApplicationContext(), "Um den QR-Code zu scannen brauchst du die Kamera", Toast.LENGTH_LONG).show();
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                    REQUEST_CAMERA);
+        } else {
+
+            // Camera permission has not been granted yet. Request it directly.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                    REQUEST_CAMERA);
+        }
+        // END_INCLUDE(camera_permission_request)
     }
     public void openNewActivity1(View view) {
         Intent intent = new Intent(this, BloodPressureBlockerActivity.class);
