9ac22f21c19c06faac85672a13d107b9231e54f4
TakedaLeyasu <c.frauendorf2000@web.de>
2016-06-16 20:35:32 +0200

    Audio hinzugefügt 1.1.3 (4)

--- app/src/main/java/com/wordpress/httpseliminatorgang/mymedicine/BloodPressureBlockerActivity.java
+++ app/src/main/java/com/wordpress/httpseliminatorgang/mymedicine/BloodPressureBlockerActivity.java
@@ -1,7 +1,10 @@
 package com.wordpress.httpseliminatorgang.mymedicine;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by Cedric on 04.06.2016.
@@ -13,5 +16,11 @@ public class BloodPressureBlockerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_blood_pressure_blocker);
 
 
+    }
+
+
+    public void PlayText1(View view) {
+        MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.blood_pressure_blocker);
+        mp.start();
     }
 }
--- app/src/main/java/com/wordpress/httpseliminatorgang/mymedicine/GoodBonesActivity.java
+++ app/src/main/java/com/wordpress/httpseliminatorgang/mymedicine/GoodBonesActivity.java
@@ -1,7 +1,9 @@
 package com.wordpress.httpseliminatorgang.mymedicine;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by Cedric on 04.06.2016.
@@ -13,5 +15,10 @@ public class GoodBonesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_good_bones);
 
 
+    }
+
+    public void PlayText2(View view) {
+        MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.good_bones);
+        mp.start();
     }
 }
