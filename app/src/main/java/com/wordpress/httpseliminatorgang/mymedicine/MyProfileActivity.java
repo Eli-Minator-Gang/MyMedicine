a4e296cb2c12c1e5f64ade86da8809a232f8f83e
TakedaLeyasu <c.frauendorf2000@web.de>
2016-06-17 15:56:59 +0200

    open in Browser hinzugefügt

--- app/src/main/java/com/wordpress/httpseliminatorgang/mymedicine/MyProfileActivity.java
+++ app/src/main/java/com/wordpress/httpseliminatorgang/mymedicine/MyProfileActivity.java
@@ -1,7 +1,10 @@
 package com.wordpress.httpseliminatorgang.mymedicine;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by Cedric on 07.06.2016.
@@ -13,5 +16,10 @@ public class MyProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_my_profile);
 
 
+    }
+
+    public void OpenBrowserProfile(View view) {
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("http://mainboardprozessor.jimdo.com/"));
+        startActivity(i);
     }
 }
